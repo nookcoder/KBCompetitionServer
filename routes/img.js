1cf0dc61f96866aef856d89e379fa2a41d12c3d1
nookcoder <gus5427@naver.com>
2021-09-25 04:55:09 +0000

    [ADD] 이미지 존재하지 않을 때 처리

--- routes/img.js
+++ routes/img.js
@@ -7,12 +7,18 @@ router.use('/:merchantId/:productName', (req, res, next) => {
     console.log(req.params);
     let imgPath = __dirname.substring(0, __dirname.length - 6) + "public/uploads/" + req.params.merchantId + req.params.productName + ".jpg";
     console.log(imgPath);
-
-    fs.readFile(imgPath, (err, data) => {
-        res.writeHead(200, { 'Content-Type': 'image/*' });
-        res.write(data);
-        res.end();
-    })
+    fs.exists(imgPath, (exists) => {
+        if (exxists) {
+            fs.readFile(imgPath, (err, data) => {
+                res.writeHead(200, { 'Content-Type': 'image/*' });
+                res.write(data);
+                res.end();
+            });
+        }
+        else {
+            console.log("사진이 없어어");
+        }
+    });
 });
 
 module.exports = router;
